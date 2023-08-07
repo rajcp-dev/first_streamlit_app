0a28bf2c0ffd7e845d163eb87484c458f60ca324
Rajendra <117044593+rajcp-dev@users.noreply.github.com>
2023-08-07 00:48:40 -0500

    indentation update

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-  streamlit.error("Please select a fruit to get the information.")
+       streamlit.error("Please select a fruit to get the information.")
   else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     #Normalize the data to display clean on the app
